6aca76530294fce6aa8c92cf51d64fb9439e9d99
soichiro-nishizono <soicchi@icloud.com>
2023-02-21 23:02:12 +0900

    fix: change signup route users/ to auth/

---
+++ pkg/router/auth.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/soicchi/chatapp_backend/pkg/controllers"
+)
+
+func addAuthRoutes(routerGroup *gin.RouterGroup) {
+	auth := routerGroup.Group("/auth")
+	{
+		auth.POST("/signup", controllers.SignUpHandler)
+	}
+}
