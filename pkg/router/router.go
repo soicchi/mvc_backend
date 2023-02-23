88a9604e4d03641c965aace1a4a8e9bac8209fae
soichiro-nishizono <soicchi@icloud.com>
2023-02-23 11:48:49 +0900

    fix: modified router package

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -5,16 +5,18 @@ import (
 )
 
 func Run() {
-	router := gin.Default()
-	setupRouter(router)
+	router := setupRouter()
 	router.Run()
 }
 
-func setupRouter(router *gin.Engine) {
+func setupRouter() *gin.Engine {
+	router := gin.Default()
 	// TODO: SetTrustedProxiesの設定を検討
 	api := router.Group("/api")
 
 	v1 := api.Group("/v1")
 	addAuthRoutes(v1)
 	// addUserRoutes(v1)
+
+	return router
 }
