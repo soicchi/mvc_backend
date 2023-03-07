5af861ccc5c703a07a23eb95763337168410c15f
soichiro-nishizono <soicchi@icloud.com>
2023-03-07 23:50:54 +0900

    fix: set logger in post controller

--- pkg/controllers/post.go
+++ pkg/controllers/post.go
@@ -7,13 +7,21 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/soicchi/chatapp_backend/pkg/models"
+	"github.com/soicchi/chatapp_backend/pkg/utils"
 )
 
+var postLogFile string = "post.log"
+
 func (handler *Handler) CreatePost(ctx *gin.Context) {
+	logger, err := utils.SetupLogger(postLogFile)
+	if err != nil {
+		panic(err)
+	}
+
 	var postInput models.CreatePostInput
 	if err := ctx.ShouldBind(&postInput); err != nil {
+		logger.Error(err)
 		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
 			"message": "Invalid request body",
 		})
 		return
@@ -26,8 +34,8 @@ func (handler *Handler) CreatePost(ctx *gin.Context) {
 	}
 	newPost, err := post.Create(handler.DB)
 	if err != nil {
+		logger.Error(err)
 		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
 			"message": "Failed to create post",
 		})
 		return
@@ -40,10 +48,15 @@ func (handler *Handler) CreatePost(ctx *gin.Context) {
 }
 
 func (handler *Handler) GetAllPosts(ctx *gin.Context) {
+	logger, err := utils.SetupLogger(postLogFile)
+	if err != nil {
+		panic(err)
+	}
+
 	posts, err := models.FindAllPosts(handler.DB)
 	if err != nil {
+		logger.Error(err)
 		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
 			"message": "Failed to get posts",
 		})
 		return
@@ -56,10 +69,15 @@ func (handler *Handler) GetAllPosts(ctx *gin.Context) {
 }
 
 func (handler *Handler) GetPost(ctx *gin.Context) {
+	logger, err := utils.SetupLogger(postLogFile)
+	if err != nil {
+		panic(err)
+	}
+
 	postId, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
+		logger.Error(err)
 		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
 			"message": "Invalid post id",
 		})
 		return
@@ -67,8 +85,8 @@ func (handler *Handler) GetPost(ctx *gin.Context) {
 
 	post, err := models.FindPostById(handler.DB, uint(postId))
 	if err != nil {
+		logger.Error(err)
 		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
 			"message": "Failed to get post",
 		})
 		return
