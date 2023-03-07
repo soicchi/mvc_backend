c97f85112e968d00f9ea57925b193ad53215da37
soichiro-nishizono <soicchi@icloud.com>
2023-03-07 23:41:51 +0900

    fix: changed output for log from only file to file and stdout

--- pkg/utils/logging.go
+++ pkg/utils/logging.go
@@ -2,24 +2,34 @@ package utils
 
 import (
 	"os"
+	"io"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func SetupLogger(logFilePath string) (*log.Logger, error) {
+func SetupLogger(logFileName string) (*log.Logger, error) {
 	logger := log.New()
 	setLoggerConfig(logger)
 
-	file, err := getLogFile(logFilePath)
+	file, err := getLogFile(logFileName)
 	if err != nil {
 		return nil, err
 	}
 
-	logger.SetOutput(file)
+	multiOutput := io.MultiWriter(file, os.Stdout)
+	logger.SetOutput(multiOutput)
 	return logger, nil
 }
 
-func getLogFile(logFilePath string) (*os.File, error) {
+func getLogFile(logFileName string) (*os.File, error) {
+	var prefix string
+	if os.Getenv("APP_ENV") == "production" {
+		prefix = "production"
+	} else {
+		prefix = "development"
+	}
+
+	logFilePath := "logs/" + prefix + "/" + logFileName
 	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		return nil, err
